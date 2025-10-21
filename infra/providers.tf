fdc9dd97e46ac777c4d9def18efb99c71f27d398
afzie99 <afzalpro9@gmail.com>
2025-10-21 20:01:08 +0530

    updating provider version

--- infra/providers.tf
+++ infra/providers.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "= 4.67.0"
     }
   }
 }
 
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+  region     = "us-east-1"
 
   endpoints {
     ec2 = "http://127.0.0.1:4566"
@@ -18,7 +18,7 @@ provider "aws" {
     s3  = "http://127.0.0.1:4566"
   }
 
-  skip_credentials_validation  = true
-  skip_metadata_api_check      = true
-  skip_requesting_account_id   = true
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 }
