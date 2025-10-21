c8baf5fdf21f23cf54901791918bbc7d248f24c3
afzie99 <afzalpro9@gmail.com>
2025-10-21 21:04:23 +0530

    changing providers block

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,21 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "= 4.67.0"
-    }
-  }
-}
-
 provider "aws" {
   access_key = "test"
   secret_key = "test"
   region     = "us-east-1"
 
   endpoints {
-    ec2 = "http://127.0.0.1:4566"
-    iam = "http://127.0.0.1:4566"
-    s3  = "http://127.0.0.1:4566"
+    ec2 = "http://host.docker.internal:4566"
+    iam = "http://host.docker.internal:4566"
+    s3  = "http://host.docker.internal:4566"
   }
 
   skip_credentials_validation = true
